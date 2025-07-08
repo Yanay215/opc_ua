8dc45df59b266ebaac994e8356e13712afaabc39
Yanay215 <149248169+Yanay215@users.noreply.github.com>
2025-07-08 17:01:29 +0500

    Add files via upload

--- test_client1/main.cpp
+++ test_client1/main.cpp
@@ -9,37 +9,47 @@
 #include <memory>
 #include <vector>
 
+template<typename T>
 void dataChangeHandler(UA_Client *client, UA_UInt32 subId, void *subContext,
                       UA_UInt32 monId, void *monContext,
                       UA_DataValue *value) {
     printf("Received data change:%i, ", value->hasValue);
     if (value->hasValue) {
-        printf("value: %f", *(double*)value->value.data);
+        printf("value: ");
+        if (value->value.type == &UA_TYPES[UA_TYPES_DOUBLE] && typeid(T) == typeid(UA_Double)) {
+            printf("%f", *(double*)value->value.data);
+        } else if (value->value.type == &UA_TYPES[UA_TYPES_INT32] && typeid(T) == typeid(UA_Int32)) {
+            printf("%i", *(int*)value->value.data);
+        } else if (value->value.type == &UA_TYPES[UA_TYPES_BOOLEAN] && typeid(T) == typeid(UA_Boolean)) {
+            printf("%i", *(bool*)value->value.data);
+        } else if (value->value.type == &UA_TYPES[UA_TYPES_STRING] && typeid(T) == typeid(UA_String)) {
+            printf("%s", (char*)value->value.data);
+        } else {
+            printf("Unknown type");
+        }
     }
     printf("\n");
 }
 
 int main(int argc, char *argv[]) {
     /* Create a client and connect */
-    UA_Client *client = UA_Client_new();
-    UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+    std::shared_ptr<UA_Client> client(UA_Client_new(), UA_Client_delete);
+    UA_ClientConfig_setDefault(UA_Client_getConfig(client.get()));
     
-    UA_StatusCode status = UA_Client_connect(client, "opc.tcp://127.0.0.1:4840");
+    UA_StatusCode status = UA_Client_connect(client.get(), "opc.tcp://127.0.0.1:4840");
     std::cout << "Connection status: 0x" << std::hex << status << std::endl;
     
     if(status != UA_STATUSCODE_GOOD) {
-        UA_Client_delete(client);
         return EXIT_FAILURE;
     }
 
     /* Create a subscription */
     UA_CreateSubscriptionRequest request = UA_CreateSubscriptionRequest_default();
-    UA_CreateSubscriptionResponse response = UA_Client_Subscriptions_create(client, request, NULL, NULL, NULL);
+    UA_CreateSubscriptionResponse response = UA_Client_Subscriptions_create(client.get(), request, NULL, NULL, NULL);
     
     if(response.responseHeader.serviceResult != UA_STATUSCODE_GOOD) {
         std::cerr << "Subscription failed: 0x" << std::hex << response.responseHeader.serviceResult << std::endl;
-        UA_CreateSubscriptionResponse_delete(&response);
-        UA_Client_delete(client);
+        UA_CreateSubscriptionResponse_clear(&response);
         return EXIT_FAILURE;
     }
     
@@ -55,28 +65,22 @@ int main(int argc, char *argv[]) {
     std::vector<UA_MonitoredItemCreateResult> results;
     UA_TimestampsToReturn t = UA_TIMESTAMPSTORETURN_BOTH;
     for (int i = 0; i < items.size(); ++i) {
-        UA_MonitoredItemCreateResult result = UA_Client_MonitoredItems_createDataChange(client, subId, t, items[i], NULL, &dataChangeHandler, NULL);
+        UA_MonitoredItemCreateResult result = UA_Client_MonitoredItems_createDataChange(client.get(), subId, t, items[i], NULL, &dataChangeHandler<UA_Double>, NULL);
         if (result.statusCode != UA_STATUSCODE_GOOD) {
             std::cerr << "Monitored item creation failed: 0x" << std::hex << result.statusCode << std::endl;
-            UA_Client_delete(client);
             return EXIT_FAILURE;
         }
         results.push_back(result);
     }
     /* Run the client for 5 seconds to process subscription messages */
     for(int i = 0; i < 30; i++) {
-        status = UA_Client_run_iterate(client, 1000);
+        status = UA_Client_run_iterate(client.get(), 1000);
         if(status != UA_STATUSCODE_GOOD) {
             std::cerr << "Client run iteration failed: 0x" << std::hex << status << std::endl;
             break;
         }
     }
     std::cout << "Client run iteration status: 0x" << std::hex << status << std::endl;
-    /* Clean up */
-    //UA_CreateSubscriptionResponse_clear(&response);
-    std::cout << "Deleting subscription with ID: " << subId << std::endl;
-    UA_Client_delete(client);
-    
     std::cout << "Goodbye" << std::endl;
     return (status == UA_STATUSCODE_GOOD) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
