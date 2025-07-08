5e623b65642ca78c0d63705f0341d14c44cb13dd
Yanay215 <149248169+Yanay215@users.noreply.github.com>
2025-07-08 14:29:54 +0500

    ading test-api and server

---
+++ test_api/build/CMakeFiles/test_api.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_api.dir/main.cpp.obj"
+  "CMakeFiles/test_api.dir/main.cpp.obj.d"
+  "libtest_api.dll.a"
+  "test_api.exe"
+  "test_api.exe.manifest"
+  "test_api.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
